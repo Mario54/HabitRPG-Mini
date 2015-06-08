cb831d1493962e75ce3086a765ea514c20bc1031
Mario Lamontagne <mariolamontagne54@gmail.com>
2015-06-08 07:28:26 -0400

    refactor app.js

--- src/js/Components/HabitRPG.js
+++ src/js/Components/HabitRPG.js
@@ -1,5 +1,12 @@
 var FluxComponent = require('flummox/component');
 var TabView = require('./TabView');
+var {DailiesView, TodosView, HabitsView } = require('./Components/Tabs');
+
+var tabs = [
+    { title: "Habits", component: HabitsView },
+    { title: "Dailies", component: DailiesView },
+    { title: "To-Dos", component: TodosView }
+];
 
 function HabitRPGFactory({React}) {
     var HabitRPG = React.createClass({
--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,8 @@
 var React = require('react');
 var FluxComponent = require('flummox/component');
-var TabView = require('./Components/TabView');
-var Tabs = require('./Components/Tabs');
-var DailiesView = Tabs.DailiesView;
-var TodosView = Tabs.TodosView;
-var HabitsView = Tabs.HabitsView;
 var AppFlux = require('./Flux');
 var HabitRPG = require('./Components/HabitRPG')({React});
 
 var flux = new AppFlux();
-
-
-
-var tabs = [
-    { title: "Habits", component: HabitsView },
-    { title: "Dailies", component: DailiesView },
-    { title: "To-Dos", component: TodosView }
-];
 
 React.render(<FluxComponent flux={flux}><HabitRPG tabs={tabs} /></FluxComponent>, document.getElementById('content'));
