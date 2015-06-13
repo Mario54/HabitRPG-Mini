193af479d36716dd34f30c034df05d926d9c08f3
Mario Lamontagne <mariolamontagne54@gmail.com>
2015-06-13 17:10:19 -0400

    lint gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,16 @@
-/*jslint node:true*/
-'use strict';
-var gulp   = require('gulp'),
-    source = require('vinyl-source-stream'),
-    gutil  = require('gulp-util'),
-    path = require('path'),
-    uglify = require('gulp-uglify'),
-    buffer = require('vinyl-buffer');
+var gulp = require("gulp"),
+    source = require("vinyl-source-stream"),
+    gutil = require("gulp-util"),
+    uglify = require("gulp-uglify"),
+    buffer = require("vinyl-buffer");
 
-gulp.task('watch', function () {
-    var watchify   = require('watchify'),
-        browserify = require('browserify'),
-        babelify   = require('babelify'),
-        getBundler = function () {
-            return watchify(browserify('./src/js/app.js', {
+gulp.task("watch", function () {
+    var watchify = require("watchify"),
+        browserify = require("browserify"),
+        babelify = require("babelify");
+
+    var getBundler = function () {
+            return watchify(browserify("./src/js/app.js", {
                 cache: {},
                 packageCache: {},
                 ignoreWatch: true,
@@ -23,29 +21,29 @@ gulp.task('watch', function () {
                 optional: ["es7.classProperties"]
             }));
         },
-        bundler   = getBundler();
+        bundler = getBundler();
 
     function rebundle() {
         var t = Date.now();
 
         return bundler
             .bundle()
-            .pipe(source('bundle.js'))
-            .pipe(gulp.dest('./dist/js'))
-            .on('end', function () {
-                gutil.log('Finished bundling after:', gutil.colors.magenta(Date.now() - t + ' ms'));
+            .pipe(source("bundle.js"))
+            .pipe(gulp.dest("./dist/js"))
+            .on("end", function () {
+                gutil.log("Finished bundling after:", gutil.colors.magenta(Date.now() - t + " ms"));
             });
     }
-    bundler.on('update', rebundle);
+    bundler.on("update", rebundle);
 
     return rebundle();
 });
 
-gulp.task('build', function () {
-    var browserify = require('browserify'),
-        babelify   = require('babelify');
+gulp.task("build", function () {
+    var browserify = require("browserify"),
+        babelify = require("babelify");
 
-    return browserify('./src/js/app.js', {
+    return browserify("./src/js/app.js", {
         cache: {},
         packageCache: {},
         ignoreWatch: true,
@@ -54,11 +52,11 @@ gulp.task('build', function () {
     }).transform(babelify, {
         optional: ["es7.classProperties"]
     }).bundle()
-        .pipe(source('bundle.js'))
+        .pipe(source("bundle.js"))
         .pipe(buffer())
         .pipe(uglify())
-        .pipe(gulp.dest('./dist/js'))
-        .on('end', function () {
-            gutil.log('Finished bundling');
+        .pipe(gulp.dest("./dist/js"))
+        .on("end", function () {
+            gutil.log("Finished bundling");
         });
 });
