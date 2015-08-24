eeda418346bd203612d7e08d232160ebda7baa66
Mario Lamontagne <mariolamontagne54@gmail.com>
2015-08-24 13:44:35 -0400

    stop webpack from transforming javascript in node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: "babel-loader" }
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
     ]
   },
   resolve: {
